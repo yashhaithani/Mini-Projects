4678ca194f7fa562f8507598a6ed944b1ad8e95c
YASH AITHANI <78597770+yashhaithani@users.noreply.github.com>
2024-05-10 16:28:42 +0530

    Add files via upload

---
+++ Bill Split Application/script.js
@@ -0,0 +1,5 @@
+function myfun() {
+    var a = document.getElementById('amount').value;
+    var b = document.getElementById('people').value;
+    document.getElementById('result').innerHTML = (a / b);
+}
